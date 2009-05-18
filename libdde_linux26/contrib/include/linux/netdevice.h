befb192dd8647442a46603d919ae4b6a9ebff74c
Eric Dumazet <dada1@cosmosbay.com>
2009-05-17 20:55:16 -0700

    commit 9d21493b4beb8f918ba248032fefa393074a5e2b
        net: tx scalability works : trans_start
    
        struct net_device trans_start field is a hot spot on SMP and high performance
        devices, particularly multi queues ones, because every transmitter dirties
        it. Is main use is tx watchdog and bonding alive checks.
    
        But as most devices dont use NETIF_F_LLTX, we have to lock
        a netdev_queue before calling their ndo_start_xmit(). So it makes
        sense to move trans_start from net_device to netdev_queue. Its update
        will occur on a already present (and in exclusive state) cache line, for
        free.
    
        We can do this transition smoothly. An old driver continue to
        update dev->trans_start, while an updated one updates txq->trans_start.
    
        Further patches could also put tx_bytes/tx_packets counters in
        netdev_queue to avoid dirtying dev->stats (vlan device comes to mind)
    
        Signed-off-by: Eric Dumazet <dada1@cosmosbay.com>
        Signed-off-by: David S. Miller <davem@davemloft.net>

--- libdde_linux26/contrib/include/linux/netdevice.h
+++ libdde_linux26/contrib/include/linux/netdevice.h
@@ -462,6 +462,10 @@ struct netdev_queue {
 	spinlock_t		_xmit_lock;
 	int			xmit_lock_owner;
 	struct Qdisc		*qdisc_sleeping;
+	/*
+	 * please use this field instead of dev->trans_start
+	 */
+	unsigned long		trans_start;
 } ____cacheline_aligned_in_smp;
 
 
@@ -801,6 +805,11 @@ struct net_device
  * One part is mostly used on xmit path (device)
  */
 	/* These may be needed for future network-power-down code. */
+
+	/*
+	 * trans_start here is expensive for high speed devices on SMP,
+	 * please use netdev_queue->trans_start instead.
+	 */
 	unsigned long		trans_start;	/* Time (in jiffies) of last Tx	*/
 
 	int			watchdog_timeo; /* used by dev_watchdog() */
@@ -1476,6 +1485,8 @@ static inline int netif_carrier_ok(const struct net_device *dev)
 {
 	return !test_bit(__LINK_STATE_NOCARRIER, &dev->state);
 }
+
+extern unsigned long dev_trans_start(struct net_device *dev);
 
 extern void __netdev_watchdog_up(struct net_device *dev);
 
