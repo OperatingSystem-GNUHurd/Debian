1a59a4fe6a62779dd462b092fea2cf5ecbfe7c74
Zheng Da <zhengda1936@gmail.com>
2009-12-03 16:46:34 +0100

    Implement panic.

--- libddekit/include/ddekit/panic.h
+++ libddekit/include/ddekit/panic.h
@@ -3,14 +3,29 @@
 
 /** \defgroup DDEKit_util */
 
+#include <stdio.h>
+
 /** Panic - print error message and enter the kernel debugger.
  * \ingroup DDEKit_util
  */
-void ddekit_panic(char *fmt, ...) __attribute__((noreturn));
+#define ddekit_panic(format, ...) do				\
+{							\
+  char buf[1024];                                       \
+  snprintf (buf, 1024, "%s", format);       \
+  fprintf (stderr , buf, ## __VA_ARGS__);		\
+  fflush (stderr);					\
+  abort ();			    \
+} while (0)
 
 /** Print a debug message.
  * \ingroup DDEKit_util
  */
-void ddekit_debug(char *fmt, ...);
+#define ddekit_debug(format, ...) do				\
+{							\
+  char buf[1024];                                       \
+  snprintf (buf, 1024, "%s: %s\n", __func__, format);       \
+  fprintf (stderr , buf, ## __VA_ARGS__);		\
+  fflush (stderr);					\
+} while (0)
 
 #endif
