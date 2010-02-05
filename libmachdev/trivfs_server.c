a839994d202a8b7fd4a93841f446bd174c7b5e11
Zheng Da <zhengda1936@gmail.com>
2010-02-05 15:16:42 +0100

    remove printing in the trivfs server.

--- libmachdev/trivfs_server.c
+++ libmachdev/trivfs_server.c
@@ -88,28 +88,22 @@ trivfs_goaway (struct trivfs_control *fsys, int flags)
 {
   int count;
 
-  fprintf (stderr, "check point 1\n");
   /* Stop new requests.  */
   ports_inhibit_class_rpcs (trivfs_cntl_portclasses[0]);
   ports_inhibit_class_rpcs (trivfs_protid_portclasses[0]);
 
   count = ports_count_class (trivfs_protid_portclasses[0]);
 
-  fprintf (stderr, "check point 2\n");
   if (count && !(flags & FSYS_GOAWAY_FORCE))
     {
-      fprintf (stderr, "check point 4\n");
       /* We won't go away, so start things going again...  */
       ports_enable_class (trivfs_protid_portclasses[0]);
       ports_resume_class_rpcs (trivfs_cntl_portclasses[0]);
       ports_resume_class_rpcs (trivfs_protid_portclasses[0]);
-      fprintf (stderr, "check point 5\n");
       return EBUSY;
     }
 
-  fprintf (stderr, "check point 3\n");
   pci_system_cleanup ();
-  fprintf (stderr, "trivfs goes away\n");
   exit (0);
 }
 
@@ -119,10 +113,8 @@ demuxer (mach_msg_header_t *inp, mach_msg_header_t *outp)
   int ret;
   extern int device_server (mach_msg_header_t *, mach_msg_header_t *);
   extern int notify_server (mach_msg_header_t *, mach_msg_header_t *);
-  fprintf (stderr, "before trivfs demuxer\n");
   ret = device_server (inp, outp) || notify_server (inp, outp)
     || trivfs_demuxer (inp, outp);
-  fprintf (stderr, "after trivfs demuxer\n");
   return ret;
 }
 
@@ -141,7 +133,6 @@ void trivfs_server()
   if (bootstrap == MACH_PORT_NULL)
     error (1, 0, "must be started as a translator");
 
-  fprintf (stderr, "after get bootstrap port\n");
   /* Reply to our parent.  */
   err = trivfs_startup (bootstrap, 0,
 			trivfs_cntl_portclasses[0], port_bucket,
@@ -149,7 +140,6 @@ void trivfs_server()
   mach_port_deallocate (mach_task_self (), bootstrap);
   if (err)
     error (1, err, "Contacting parent");
-  fprintf (stderr, "start trivfs\n");
 
   /* Launch.  */
   do
